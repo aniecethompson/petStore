e338df0661961627145e6eb00e9eaf6bbd401a39
github username <github email address>
2020-02-04 18:40:21 -0500

    added react-routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Switch, Route} from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Cart from './components/Cart';
+import ProductList from './components/ProductList';
+import Default from './components/Default';
+import Details from './components/Details';
+
 
 function App() {
   return (
     <React.Fragment>
-      <h3>hello from app</h3>
+      <NavBar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details}/>
+          <Route path="/cart" component={Cart}/>
+          <Route component={Default}/>
+        </Switch>
+      
+     
+      {/* <Default /> */}
     </React.Fragment>
   );
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Product from './Product';
+// import Product from './Product';
 
 class ProductList extends Component {
     render() {
