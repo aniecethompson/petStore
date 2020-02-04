72fc10b06104b9250baf00ca10b7681c11d48207
github username <github email address>
2020-02-04 15:50:57 -0500

    created components

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div>
+               <h3>Hello from Nav Bar</h3> 
+            </div>
+        );
+    }
+}
+
+export default NavBar;
---
+++ src/components/ProductList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Product from './Product';
+
+class ProductList extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello from Product List</h3>
+            </div>
+        );
+    }
+}
+
+export default ProductList;
