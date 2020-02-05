36b2e85e289bf73082355a310ebf6536c1b0bdc6
github username <github email address>
2020-02-04 20:12:07 -0500

    completed navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+// import logo from './logo.svg';
+import {Link} from 'react-router-dom'
 
 class NavBar extends Component {
     render() {
         return (
-            <div>
-               <h3>Hello from Nav Bar</h3> 
-            </div>
+           <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+
+            <Link to="/">
+                <img src="../logo.svg" alt="logo"
+                className="navbar-brand"/>
+            </Link>
+            <ul className="navbar-nav align-items-center">
+                <li className="nav-item ml-5"></li>
+                <Link to="/" className="nav-link">
+                    Products
+                </Link>
+            </ul>
+            <Link to="/cart" className="ml-auto">
+                <button>
+                    <i className="fas fa-cart-plus"/>
+                    My Cart
+                </button>
+            </Link>
+           </nav>
         );
     }
 }
